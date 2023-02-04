3c2776dc1ca444812c30b5bc003fc572119cf782
pavel-1380 <pavel-1380@yandex.ru>
2023-02-04 12:10:27 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const calculator = document.createElement('div');
+calculator.classList.add('calculator');
+document.body.append(calculator);
+
+const result = document.createElement('output');
+result.classList.add('result');
+
+const buttons = ['back', 'c', '*', '/', '7', '8', '9', '+',
+'4', '5', '6', '-', '1', '2', '3', '.', '0', '='];
+
